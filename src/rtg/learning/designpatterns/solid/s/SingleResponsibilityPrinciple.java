c1db0c4b90ba9e46f983602d316a272cb8d0948c
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-08 17:41:45 +0530

    Breaking SRP by adding save() & load() methods

--- src/rtg/learning/designpatterns/solid/s/SingleResponsibilityPrinciple.java
+++ src/rtg/learning/designpatterns/solid/s/SingleResponsibilityPrinciple.java
@@ -1,5 +1,8 @@
 package rtg.learning.designpatterns.solid.s;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +22,18 @@ class Journal{
 	public String toString() {
 		return String.join(System.lineSeparator(), entries);
 	}
+	
+	public void save(String filename) throws FileNotFoundException{
+		try(PrintStream out=new PrintStream(filename)){
+			out.println(toString());
+		}
+	}
+	public void load(String filename) {	}
+	public void load(URL url) {}
 }
 
 public class SingleResponsibilityPrinciple {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Journal j=new Journal();
 		j.addEntry("Learn Design Pattern");
 		j.addEntry("Learning Single Responsibility Principle");
