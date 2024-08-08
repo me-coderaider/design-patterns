e764eb1cf028e9e48b2d75b8ee949e95d0f85b08
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-08 19:22:19 +0530

    Breaking OCP by making change in ProductFilter class

---
+++ src/rtg/learning/designpatterns/solid/o/OpenClosedPrinciple.java
@@ -0,0 +1,57 @@
+package rtg.learning.designpatterns.solid.o;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+enum Color{
+	RED, GREEN, BLUE
+}
+
+enum Size{
+	SMALL, MEDIUM, LARGE, HUGE
+}
+
+class Product{
+	public String name;
+	public Color color;
+	public Size size;
+	
+	public Product(String name, Color color, Size size) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.size = size;
+	}
+}
+
+class ProductFilter{
+	public Stream<Product> filterByColor(List<Product> products, Color color){
+		return products.stream().filter(p -> p.color == color);
+	}
+	
+	public Stream<Product> filterBySize(List<Product> products, Size size){
+		return products.stream().filter(p -> p.size == size);
+	}
+	
+	public Stream<Product> filterBySizeAndColor(List<Product> products, Size size, Color color){
+		return products.stream().filter(p -> p.size == size && p.color==color);
+	}
+}
+
+public class OpenClosedPrinciple {
+	
+	public static void main(String[] args) {
+		
+	
+	Product apple=new Product("Apple", Color.GREEN, Size.SMALL);
+	Product tree=new Product("Tree", Color.GREEN, Size.LARGE);
+	Product house=new Product("House", Color.BLUE, Size.LARGE);
+	
+	List<Product> products=List.of(apple, tree, house);
+	
+	ProductFilter pf=new ProductFilter();
+	System.out.println("Green Products (old):");
+	pf.filterByColor(products, Color.GREEN)
+		.forEach(p -> System.out.println(" - "+p.name+ " is green") );
+	}
+}
