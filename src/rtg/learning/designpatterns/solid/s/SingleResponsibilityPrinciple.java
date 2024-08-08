df2339330a6547cd0f45f17881fcadf258ca4f1a
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-08 17:24:15 +0530

    SRP Maintained

--- src/rtg/learning/designpatterns/solid/s/SingleResponsibilityPrinciple.java
+++ src/rtg/learning/designpatterns/solid/s/SingleResponsibilityPrinciple.java
@@ -1,5 +1,31 @@
 package rtg.learning.designpatterns.solid.s;
 
-public class SingleResponsibilityPrinciple {
+import java.util.ArrayList;
+import java.util.List;
 
+class Journal{
+	private final List<String> entries=new ArrayList<>();
+	private static int count=0;
+	
+	public void addEntry(String text) {
+		entries.add(""+ (++count)+": "+text);
+	}
+	
+	public void removeEntry(int index) {
+		entries.remove(index);
+	}
+
+	@Override
+	public String toString() {
+		return String.join(System.lineSeparator(), entries);
+	}
+}
+
+public class SingleResponsibilityPrinciple {
+	public static void main(String[] args) {
+		Journal j=new Journal();
+		j.addEntry("Learn Design Pattern");
+		j.addEntry("Learning Single Responsibility Principle");
+		System.out.println(j);
+	}
 }
