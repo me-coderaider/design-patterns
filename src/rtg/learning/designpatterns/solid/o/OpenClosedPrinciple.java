6bfc0fac9213f24b31a855367e6080d7af133483
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-09 11:50:55 +0530

    maintaining OCP using 2 interface Filter & Substitution

--- src/rtg/learning/designpatterns/solid/o/OpenClosedPrinciple.java
+++ src/rtg/learning/designpatterns/solid/o/OpenClosedPrinciple.java
@@ -38,6 +38,70 @@ class ProductFilter{
 	}
 }
 
+interface Specification<T>{
+	boolean isSatisfied(T item);
+}
+
+interface Filter<T>{
+	Stream<T> filter(List<T> items, Specification<T> spec);	
+}
+
+class BetterFilter implements Filter<Product>{
+
+	@Override
+	public Stream<Product> filter(List<Product> items, Specification<Product> spec) {
+		return items.stream().filter(p -> spec.isSatisfied(p));
+	}
+	
+}
+
+class ColorSpecification implements Specification<Product>{
+	private Color color;
+	
+	public ColorSpecification(Color color) {
+		super();
+		this.color = color;
+	}
+
+	@Override
+	public boolean isSatisfied(Product item) {
+		return item.color==color;
+	}
+	 
+}
+
+class SizeSpecification implements Specification<Product>{
+	private Size size;
+	
+	public SizeSpecification(Size size) {
+		super();
+		this.size = size;
+	}
+	
+	@Override
+	public boolean isSatisfied(Product item) {
+		return item.size==size;
+	}
+	 
+}
+
+class AndSpecification<T> implements Specification<T>{
+
+	private Specification<T> first, second;
+	
+	public AndSpecification(Specification<T> first, Specification<T> second) {
+		super();
+		this.first = first;
+		this.second = second;
+	}
+
+	@Override
+	public boolean isSatisfied(T item) {
+		return first.isSatisfied(item) && second.isSatisfied(item);
+	}
+	
+}
+
 public class OpenClosedPrinciple {
 	
 	public static void main(String[] args) {
@@ -53,5 +117,15 @@ public class OpenClosedPrinciple {
 	System.out.println("Green Products (old):");
 	pf.filterByColor(products, Color.GREEN)
 		.forEach(p -> System.out.println(" - "+p.name+ " is green") );
+	
+	BetterFilter bf=new BetterFilter();
+	System.out.println("Green Products (new):");
+	bf.filter(products,new ColorSpecification(Color.GREEN))
+		.forEach(p -> System.out.println(" - "+p.name+ " is green"));
+	
+	System.out.println("Large Blue items");
+	bf.filter(products, new AndSpecification<>(new ColorSpecification(Color.BLUE), new SizeSpecification(Size.LARGE)))
+	.forEach(p -> System.out.println(" - "+p.name+ " is large and blue "));
+	
 	}
 }
