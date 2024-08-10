2abffe72c5dde10fc140e79377dc541ae02a09b8
me-coderaider <kashyappankajnsp@gmail.com>
2024-08-10 12:48:27 +0530

    Maintaining ISP by separate interface & by combining multiple interfaces

--- src/rtg/learning/designpatterns/solid/i/InterfaceSegregationPrinciple.java
+++ src/rtg/learning/designpatterns/solid/i/InterfaceSegregationPrinciple.java
@@ -10,6 +10,59 @@ interface Machine{
 	void scan(Document d);
 }
 
+interface Printer{
+	void print(Document d);
+}
+
+interface Scanner{
+	void scan(Document d);
+}
+
+interface Fax{
+	void fax(Document d);
+}
+
+class JustAPrinter implements Printer{
+
+	@Override
+	public void print(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+class Photocopier implements Printer, Scanner{
+
+	@Override
+	public void scan(Document d) {
+		
+	}
+
+	@Override
+	public void print(Document d) {
+		
+	}
+	
+}
+
+interface MultiFunctionDevice extends Printer, Scanner{ }
+
+class MultiFunctionMachine implements MultiFunctionDevice{
+
+	@Override
+	public void print(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scan(Document d) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
 class MultiFunctionPrinter implements Machine{
 
 	@Override
