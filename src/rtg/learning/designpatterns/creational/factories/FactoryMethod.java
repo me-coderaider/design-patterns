8241075c9ce71da46349e18c85792909474cc353
me-coderaider <kashyappankajnsp@gmail.com>
2024-09-23 22:02:58 +0530

    factories changes

---
+++ src/rtg/learning/designpatterns/creational/factories/FactoryMethod.java
@@ -0,0 +1,25 @@
+package rtg.learning.designpatterns.creational.factories;
+
+class Point{
+	private double x,y;
+
+	public Point(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+//	public Point(double rho, double theta) {
+//		x = rho*Math.cos(theta);
+//		Y = rho*Math.sin(theta);
+//	}
+	
+}
+
+public class FactoryMethod {
+
+	public static void main(String[] args) {
+
+	}
+
+}
